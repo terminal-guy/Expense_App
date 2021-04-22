d6fdd713819644f2d22e3f3382c540c1661d45f4
terminal-guy <vishaladhithya19@gmail.com>
2021-04-22 15:54:43 +0530

    making strin interpolation to work

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,43 @@ class MyHomePage extends StatelessWidget {
               children: transactions.map(
                 (tx) {
                   return Card(
-                    child: Text(tx.title),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          )),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            '\$${tx.amount}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              tx.date.toString(),
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   );
                 },
               ).toList(),
