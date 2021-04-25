34dbf5a4475d50fce1ce2a6715e4703af37191d4
terminal-guy <vishaladhithya19@gmail.com>
2021-04-25 11:27:03 +0530

    configuring themes with ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+      ),
       home: MyHomePage(),
     );
   }
@@ -65,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense App'),
+        title: Text('Personal Expense'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
